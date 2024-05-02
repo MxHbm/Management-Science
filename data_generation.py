18991cde1f469368f59bcd2e08fd4666c38cbe8a
Max Hubmann <maxhubmann@outlook.com>
2024-05-02 14:01:02 +0200

    added the parameter strcuture for crteating paramter objects

---
+++ data_generation.py
@@ -0,0 +1,191 @@
+### Script for generating data fpr our model
+
+class Parameters:
+
+    def __init__(self, T: list, F: list, S: list, FT: list, MP: list, CT: list, L: list):
+
+        # Sets for creating the parameters
+        self.T = T
+        self.F = F
+        self.S = S
+        self.FT = FT
+        self.MP = MP
+        self.CT = CT
+        self.L = L
+
+        #First definition of parameters and call for implementing them
+        self.fty = self.create_fty()
+        self.cty = self.create_cty()
+        self.fpr = self.create_fpr()
+        self.fy = self.create_fy()
+        self.rsc = self.create_rsc()
+        self.roc = self.create_roc()
+        self.el = self.create_el()
+        self.tau = self.create_tau()
+        self.i0f = self.create_i0f()
+        self.tl_min = self.create_tl_min()
+        self.tl_max = self.create_tl_max()
+        self.r0 = self.create_r0()
+        self.r_max = self.create_r_max()
+        self.dmax = self.create_dmax()
+        self.cmin = self.create_cmin()
+        self.cmax = self.create_cmax()
+        self.alpha = self.create_alpha()
+        self.ost = self.create_ost()
+        self.wp = self.create_wp()
+        self.el_min = self.create_el_min()
+        self.el_max = self.create_el_max()
+        self.is_ = self.create_is()
+        self.omega_fw = self.create_omega_fw()
+        self.omega_dc = self.create_omega_dc()
+        self.rr = self.create_rr()
+        self.r = self.create_r()
+        self.re = self.create_re()
+        self.imax = self.create_imax()
+        self.zmax = self.create_zmax()
+        self.sc = self.create_sc()
+        self.beta = self.create_beta()
+        self.sigma = self.create_sigma()
+        self.iwip0 = self.create_iwip0()
+        self.tc = self.create_tc()
+        self.sco = self.create_sco()
+
+    def create_fty(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_cty(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_fpr(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_fy(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_rsc(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_roc(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_el(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_tau(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_i0f(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_tl_min(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_tl_max(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_r0(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_r_max(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_dmax(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_cmin(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_cmax(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_alpha(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_ost(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_wp(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_el_min(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_el_max(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_is(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_omega_fw(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_omega_dc(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_rr(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_r(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_re(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_imax(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_zmax(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_sc(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_beta(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_sigma(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_iwip0(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_tc(self) -> list:
+        ''' Add description of the function here '''
+        pass
+
+    def create_sco(self) -> list:
+        ''' Add description of the function here '''
+        pass
