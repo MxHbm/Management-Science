4763297da9678cfdc20386f64c2e806fb22e7f40
Max Hubmann <maxhubmann@outlook.com>
2024-05-02 20:09:48 +0200

    first addings

--- model.py
+++ model.py
@@ -1,43 +1,24 @@
 #### Load necessary packages 
 
-
-
 #### Import python scrips
-from data_generation import *
+from data_generation import *  #Werte für Parameter
+import globals #Globale variablen
 
-# Defining the parameters for the model
-# Planungshorizont (days)
-T_end = 30
+def main():
 
-# Produktfamilien 
-F_end = 4
+    # Erzeuge Sets für das Gurobi Modell
+    T = [i for i in range(globals.T_end)]
+    F = [i for i in range(globals.F_end)]
+    S = [i for i in range(globals.S_end)]
+    FT = globals.FT_values
+    MP = [i for i in range(globals.MP_end)]
+    CT = globals.CT_values
+    L = [i for i in range(globals.L_end)]
 
-# Scenarios 
-S_end = 243
+    ### Get the needed parameters
 
-# Family types
-FT_values = [0, 1]
+    data = Parameters_FirstStage(T,F,S,FT,MP,CT,L)
 
-# Manufacturing Plants
-MP_end = 4
+    print(data.cmin)
 
-# Produktion camp. types
-CT_values = [0, 1]
-
-# Distribution centers
-L_end = 9
-
-# Erzeuge Sets für das Gurobi Modell - eventuell als load function schreiben?
-T = [i for i in range(T_end)]
-F = [i for i in range(F_end)]
-S = [i for i in range(S_end)]
-FT = FT_values
-MP = [i for i in range(MP_end)]
-CT = CT_values
-L = [i for i in range(L_end)]
-
-### Get the needed parameters
-
-data = Parameters(T,F,S,FT,MP,CT,L)
-
-print(data.cmin)
+main()
